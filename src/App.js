02b2700f06df9a1ce5b103f705bc89faa823834d
ShasheeshPurohit <75235962+ShasheeshPurohit@users.noreply.github.com>
2020-12-10 09:46:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var tvShowsDB = {
+  comedy: [
+    {
+      name: "How I Met Your Mother",
+      rating: "7/10"
+    },
+    {
+      name: "Fresh Prince of Bel Air",
+      rating: "8/10"
+    }
+  ],
+
+  drama: [
+    {
+      name: "Suits",
+      rating: "8/10"
+    },
+    {
+      name: "Mr. Robot",
+      rating: "7/10"
+    }
+  ],
+
+  action: [
+    {
+      name: "Flash",
+      rating: "7/10"
+    },
+    {
+      name: "The Boys",
+      rating: "6/10"
+    }
+  ]
+};
+
+var showList = Object.keys(tvShowsDB);
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("comedy");
+
+  function genreHandler(genreName) {
+    setGenre(genreName);
+  }
+
+  return (
+    <div className="App">
+      <h1 className="hero-heading">🎭 TV Shows</h1>
+      <div>
+        {showList.map((genre) => {
+          return (
+            <button className="genre-btn" onClick={() => genreHandler(genre)}>
+              {genre}
+            </button>
+          );
+        })}
+      </div>
+
+      <div>
+        <ul className="genre-list">
+          {tvShowsDB[selectedGenre].map((show) => {
+            return (
+              <li className="genre-list-item">
+                <div className="showName">{show.name}</div>
+                <div className="rating">rating: {show.rating}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
